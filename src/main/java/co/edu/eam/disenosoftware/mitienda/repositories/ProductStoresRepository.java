6149cae2383c2f98cf3d15aa5d88d87e0667ef86
JoseHurtadoVGitHub <jose.hurtado.9321@eam.edu.com>
2020-11-03 17:13:53 -0500

    1.0 getAllProductStoreByStoreId

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ProductStoresRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ProductStoresRepository.java
@@ -1,8 +1,41 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
+import co.edu.eam.disenosoftware.mitienda.exceptions.APIException;
+import co.edu.eam.disenosoftware.mitienda.model.entities.ProductStore;
+import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
+import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
+import co.edu.eam.disenosoftware.mitienda.webservices.ProductStoreAPIClient;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Repository to access productstores data
  */
 public class ProductStoresRepository {
+
+  /**
+   * Get all Product Store By Id Store
+   *
+   */
+  public List<ProductStore> getAllProductStoreByStoreId(Long storeId) throws IOException {
+
+    ProductStoreAPIClient apiClient= RetroFitUtils.buildAPIClient(ProductStoreAPIClient.class);
+
+    Call<List<ProductStore>> request = apiClient.getAllProductStoreByStoreId(storeId);
+    Response<List<ProductStore>> response = request.execute();
+
+    if(response.isSuccessful()) {
+      return response.body();
+    }else{
+      throw APIErrorHandler.throwApiException(response);
+
+    }
+
+  }
+
+
 
 }
--- src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ProductStoreAPIClient.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ProductStoreAPIClient.java
@@ -1,8 +1,20 @@
 package co.edu.eam.disenosoftware.mitienda.webservices;
 
+import co.edu.eam.disenosoftware.mitienda.model.entities.ProductStore;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
 /**
  * ProductStore api web service client
  */
 public interface ProductStoreAPIClient {
+
+    public static final String PRODUCT_STORE_URL= "api/products-store/";
+
+    @GET(PRODUCT_STORE_URL + "stores/{storeId}/products")
+    Call<List<ProductStore>> getAllProductStoreByStoreId(@Path("storeId")Long storeId);
 
 }
