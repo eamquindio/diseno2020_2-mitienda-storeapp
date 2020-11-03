a464a4191649d054da10b51b7c556e9dd5843e41
SebastianObando <sebas.obando1724@gmail.com>
2020-11-03 16:24:32 -0500

    rebase

--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/OrderProductRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/OrderProductRepository.java
@@ -1,8 +1,51 @@
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
+import co.edu.eam.disenosoftware.mitienda.exceptions.APIException;
+import co.edu.eam.disenosoftware.mitienda.model.responses.ErrorResponse;
+import co.edu.eam.disenosoftware.mitienda.util.APIErrorHandler;
+import co.edu.eam.disenosoftware.mitienda.util.RetroFitUtils;
+import co.edu.eam.disenosoftware.mitienda.webservices.OrderProductAPIClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+
 /**
  * Repository to access Order product data
  */
 public class OrderProductRepository {
+
+  public void deleteOrderProductById (Long orderProductId) throws  IOException{
+    //crear el cliente del api - crear quien hace el requst
+    OrderProductAPIClient apiClient = RetroFitUtils.buildAPIClient(OrderProductAPIClient.class);
+
+    //hacer el request
+    Call<Void> request = apiClient.deleteOrderProductById(orderProductId);
+    Response<Void> response = request.execute();
+
+    //procesar la respuesta
+    if (!(response.isSuccessful())){
+      throw APIErrorHandler.throwApiException(response);
+    }
+  }
+
+  public static void main(String[] args){
+    OrderProductRepository repo = new OrderProductRepository();
+    try {
+      repo.deleteOrderProductById(127L);
+
+    } catch (APIException e) {
+      System.out.println(e.getMessage());
+      System.out.println(e.getStatus());
+      System.out.println(e.getErrorCode());
+      e.printStackTrace();
+
+    }catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
--- src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/repositories/ShoppingCartRepository.java
@@ -1,3 +1,4 @@
+
 package co.edu.eam.disenosoftware.mitienda.repositories;
 
 /**
