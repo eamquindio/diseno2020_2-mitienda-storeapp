8f8db7d06fa1f67de60b68d73f95aa7c9219c1d5
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-11-04 21:50:10 -0500

    fix some error

--- src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ProductStoreAPIClient.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/webservices/ProductStoreAPIClient.java
@@ -3,6 +3,7 @@ package co.edu.eam.disenosoftware.mitienda.webservices;
 
 import co.edu.eam.disenosoftware.mitienda.model.entities.ProductStore;
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.List;
 public interface ProductStoreAPIClient {
 
   public static final String PRODUCT_STORE_URL= "api/products-store/";
- Call<List<ProductStore>> getAllProductStoreByStoreId(@Path("storeId")Long storeId);
+
+  @GET(PRODUCT_STORE_URL + "stores/{storeId}/products")
+  Call<List<ProductStore>> getAllProductStoreByStoreId(@Path("storeId")Long storeId);
 
 }
