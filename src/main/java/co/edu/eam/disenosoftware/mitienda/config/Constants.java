438ac4eaed671a04017967f0a39ea5dd1eaec770
Camilo Andres Ferrer bustos <caferrerb@MacBook-Pro-de-Camilo-2.local>
2020-11-13 07:24:09 -0500

    constants

---
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/config/Constants.java
@@ -0,0 +1,20 @@
+package co.edu.eam.disenosoftware.mitienda.config;
+
+import java.awt.*;
+
+/**
+ * Generic constants
+ */
+public class Constants {
+
+  /**
+   * products Base url
+   */
+  public static final String PRODUCT_IMAGE_URL = "https://raw.githubusercontent.com/eamquindio/mitiendacdn/main/images/products/";
+
+  /**
+   * oficcial green
+   */
+  private static final Color COLOR_GREEN = new Color(122,191,181);
+
+}
