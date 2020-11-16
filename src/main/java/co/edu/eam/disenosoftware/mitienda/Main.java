eae1e78c8ff0214d07b2ec4baf668717d96b7ea8
CesarPava <cesarluis040302@gmail.com>
2020-11-15 23:01:56 -0500

    Corrigiendo errores

--- src/main/java/co/edu/eam/disenosoftware/mitienda/Main.java
+++ src/main/java/co/edu/eam/disenosoftware/mitienda/Main.java
@@ -19,9 +19,13 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+<<<<<<< HEAD
     Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
     LocalStorage.saveData("orderId", 1l);
     Navigator.goToFrame("StoreOrderPage");
+=======
+    // TODO code application logic here
+>>>>>>> 65c65fe... Corrigiendo errores
   }
 
 }
